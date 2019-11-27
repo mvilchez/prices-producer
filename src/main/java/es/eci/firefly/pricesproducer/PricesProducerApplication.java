d94e11686ffa0ee2ea045d30d199a3a56c1feee2
marino <marino.vilchez@gmail.com>
2019-11-27 15:04:48 +0100

    Cambiamos el parent Añadimos las clases de utilidad de fechas

--- src/main/java/es/eci/firefly/pricesproducer/PricesProducerApplication.java
+++ src/main/java/es/eci/firefly/pricesproducer/PricesProducerApplication.java
@@ -3,12 +3,15 @@ package es.eci.firefly.pricesproducer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import es.eci.firefly.pricesproducer.model.Price;
 import es.eci.firefly.pricesproducer.service.Producer;
+import es.eci.firefly.pricesproducer.utils.DateUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -16,12 +19,12 @@ import java.util.Arrays;
 public class PricesProducerApplication implements CommandLineRunner {
 
     private final Producer producer;
-    private final ObjectMapper mapper;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    PricesProducerApplication(Producer producer, ObjectMapper mapper) {
+    PricesProducerApplication(Producer producer) {
         this.producer = producer;
-        this.mapper = mapper;
+        this.objectMapper = new ObjectMapper();
     }
 
     public static void main(String[] args) {
@@ -32,8 +35,9 @@ public class PricesProducerApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         log.info("Application started with command-line arguments: {} . \n To kill this application, press Ctrl + C.",
                 Arrays.toString(args));
-        Price priceXboxPesetas = Price.builder().item("Xbox 360").company("ECI").value(44999.00).
-                commercialCenterCode("232").build();
-        this.producer.sendMessage(mapper.writeValueAsString(priceXboxPesetas));
+        Price priceXboxPesetas = Price.builder().item("Xbox 360").company("ECI").value(44999.00).priceCreationDate(
+                DateUtils.asDate(LocalDateTime.now())).commercialCenterCode("232")
+                .build();
+        this.producer.sendMessage(objectMapper.writeValueAsString(priceXboxPesetas));
     }
 }
--- src/test/java/es/eci/firefly/pricesproducer/PricesProducerApplicationTests.java
+++ src/test/java/es/eci/firefly/pricesproducer/PricesProducerApplicationTests.java
@@ -1,6 +1,6 @@
 package es.eci.firefly.pricesproducer;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
